bc3755dca75db1ed9e20ade275018946a41265f9
alowe17 <p.danil51007@gmail.com>
2026-03-21 09:31:00 +0300

    Создан service DailyResultService

--- src/main/java/com/example/sber/repository/DailyResultRepository.java
+++ src/main/java/com/example/sber/repository/DailyResultRepository.java
@@ -1,13 +1,13 @@
 package com.example.sber.repository;
 
 import com.example.sber.model.entity.DailyResult;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
-public interface DailyResultRepository {
+public interface DailyResultRepository extends JpaRepository<DailyResult,Long> {
 
     List<DailyResult> findByEmployeeId(Long employeeId);
 
